0c0a27cb2577afb5bcc875d4babce397a23ca510
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-02-09 11:02:06 +0000

    modificacion

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
-let domain = ['.com', 'es']
 
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
     for (let s = 0; s < noun.length; s++) {
-        for (let d = 0; d < domain.length; d++) {
-        console.log(pronoun[i]+adj[j]+noun[s]+domain[d]);
-    }
+    console.log(pronoun[i]+adj[j]+noun[s] + ".com");
 }
 }
 }
